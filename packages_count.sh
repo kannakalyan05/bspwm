f1e7364539d5c59b43b0e7d160ef9b4d5ca4b075
Kanna kalyan <kannakalyan05@gmail.com>
2023-10-23 16:24:16 +0530

    Last updated on 23/10/2023 16:24:15

--- packages_count.sh
+++ packages_count.sh
@@ -2,7 +2,6 @@
 
 # Define the list of packages
 packages=(
-  neovim
   network-manager-applet
   dunst
   libnotify
@@ -24,10 +23,12 @@ packages=(
   thunar
 )
 
+# Initialize variables to keep track of total dependencies and package dependencies
+total_dependencies=0
+declare -A package_dependencies
+
 # Loop through the packages and check their dependencies
 for package in "${packages[@]}"; do
-  echo "Checking dependencies for package: $package"
-  
   # Use yay to get package info and filter the "Depends On" line
   depends_on=$(yay -Qi "$package" | grep -i 'depends on' | cut -d ':' -f2-)
   
@@ -40,8 +41,18 @@ for package in "${packages[@]}"; do
     dependency_count=0
   fi
   
-  # Print the number of dependencies
-  echo "Number of dependencies: $dependency_count"
-  echo
+  # Add the dependency count to the total
+  total_dependencies=$((total_dependencies + dependency_count))
+  
+  # Store the package name and its dependency count
+  package_dependencies["$package"]=$dependency_count
+done
+
+# Print the total number of dependencies at the end
+echo "Total number of dependencies for all packages: $total_dependencies"
+
+# Print the list of packages and their respective dependencies
+for package in "${!package_dependencies[@]}"; do
+  echo "$package: ${package_dependencies[$package]}"
 done
 
